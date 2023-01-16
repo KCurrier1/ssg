2a2f1debfc72f809572ce96ec91a15867b18fe86
Kayla Currier <kcurrier1@my.gcu.edu>
2023-01-15 20:17:10 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +21,26 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+/* Color Theme Swatches in Hex */
+$SSG-deep-1-hex: #731062;
+$SSG-deep-2-hex: #A66BBF;
+$SSG-deep-3-hex: #2B4075;
+$SSG-deep-4-hex: #F2F2F2;
+$SSG-deep-5-hex: #0D0D0D;
+
+/* Color Theme Swatches in RGBA */
+$SSG-deep-1-rgba: rgba(115,16,98, 1);
+$SSG-deep-2-rgba: rgba(166,107,191, 1);
+$SSG-deep-3-rgba: rgba(43,64,117, 1);
+$SSG-deep-4-rgba: rgba(242,242,242, 1);
+$SSG-deep-5-rgba: rgba(13,13,13, 1);
+
+/* Color Theme Swatches in HSLA */
+$SSG-deep-1-hsla: hsla(310, 75, 25, 1);
+$SSG-deep-2-hsla: hsla(282, 39, 58, 1);
+$SSG-deep-3-hsla: hsla(222, 46, 31, 1);
+$SSG-deep-4-hsla: hsla(0, 0, 94, 1);
+$SSG-deep-5-hsla: hsla(0, 0, 5, 1);
+
 </style>
